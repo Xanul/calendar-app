5506ff07cec6f27886ee5840fdb5070fa5644c1b
Xanul <rivas.060@gmail.com>
2022-11-14 20:06:39 -0600

    Added useForm custom hook to handle LoginPage inputs

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,78 +1,116 @@
-import './LoginPage.css';
+import { useForm } from "../../hooks/useForm";
+import "./LoginPage.css";
+
+const loginFormFields = {
+  loginEmail: "",
+  loginPassword: "",
+};
+
+const registerFormFields = {
+  registerName: "",
+  registerEmail: "",
+  registerPassword: "",
+  registerPassword2: "",
+};
 
 export const LoginPage = () => {
+
+  const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields);
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    console.log({loginEmail, loginPassword});
+  }
+
+  const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange } = useForm(registerFormFields)
+  const registerSubmit = (event) => {
+    event.preventDefault();
+    console.log({ registerName, registerEmail, registerPassword, registerPassword2});
+  }
+
+
   return (
     <div className="container login-container">
-            <div className="row">
-                <div className="col-md-6 login-form-1">
-                    <h3>Ingreso</h3>
-                    <form>
-                        <div className="form-group mb-2">
-                            <input 
-                                type="text"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña"
-                            />
-                        </div>
-                        <div className="d-grid gap-2">
-                            <input 
-                                type="submit"
-                                className="btnSubmit"
-                                value="Login" 
-                            />
-                        </div>
-                    </form>
-                </div>
-
-                <div className="col-md-6 login-form-2">
-                    <h3>Registro</h3>
-                    <form>
-                        <div className="form-group mb-2">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Nombre"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña" 
-                            />
-                        </div>
-
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Repita la contraseña" 
-                            />
-                        </div>
-
-                        <div className="d-grid gap-2">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
-                                value="Crear cuenta" />
-                        </div>
-                    </form>
-                </div>
+      <div className="row">
+        <div className="col-md-6 login-form-1">
+          <h3>Ingreso</h3>
+          <form onSubmit={ loginSubmit }>
+            <div className="form-group mb-2">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Correo"
+                name="loginEmail"
+                value={ loginEmail }
+                onChange={ onLoginInputChange }
+              />
             </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                name="loginPassword"
+                value={ loginPassword }
+                onChange={ onLoginInputChange }
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Login" />
+            </div>
+          </form>
         </div>
-  )
-}
+
+        <div className="col-md-6 login-form-2">
+          <h3>Registro</h3>
+          <form onSubmit={ registerSubmit }>
+            <div className="form-group mb-2">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre"
+                name="registerName"
+                value={ registerName }
+                onChange={ onInputChange }
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Correo"
+                name="registerEmail"
+                value={ registerEmail }
+                onChange={ onInputChange }
+              />
+            </div>
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                name="registerPassword"
+                value={ registerPassword }
+                onChange={ onInputChange }
+              />
+            </div>
+
+            <div className="form-group mb-2">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repita la contraseña"
+                name="registerPassword2"
+                value={ registerPassword2 }
+                onChange={ onInputChange }
+              />
+            </div>
+
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Crear cuenta" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
