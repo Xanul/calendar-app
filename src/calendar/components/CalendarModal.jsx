3c908a4977fa0f729c1e811f92913a095710c202
Xanul <rivas.060@gmail.com>
2022-11-23 14:03:47 -0600

    Added function to update existing event in DB

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -88,8 +88,6 @@ export const CalendarModal = () => {
 
     if( formValues.title.length <= 0 ) return;
     
-    console.log(formValues);
-    
     await startSavingEvent( formValues )
     closeDateModal()
     setFormSubmitted(false)
--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
+import Swal from "sweetalert2";
 import calendarApi from "../api/calendarApi";
 import { convertEventsToDate } from "../helpers";
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
@@ -20,18 +21,23 @@ export const useCalendarStore = () => {
 
   const startSavingEvent = async (calendarEvent) => {
     
-    if( calendarEvent._id ){
-      // Actualizando
-      dispatch( onUpdateEvent({ ...calendarEvent }) )
-    } else {
-      // Creando
-      
+    try {
+      // Actualizando un evento
+      if( calendarEvent.id ){
+        await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent)
+        dispatch( onUpdateEvent({ ...calendarEvent, user }));
+        return 
+      }
+      // Creando un evento
       const { data } = await calendarApi.post('/events', calendarEvent)
-      console.log({data});
-
       dispatch( onAddNewEvent({...calendarEvent, id: data.event.id, user}));
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error saving event', error.response.data.msg, 'error')
     }
-
+    
+    
+    
   }
 
   const startLoadingEvents = async () => {
