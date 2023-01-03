aa0278db9fd026f07f636d79d72332531928121f
Xanul <rivas.060@gmail.com>
2023-01-02 22:30:59 -0600

    Added the config for testing and initial test

---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,21 @@
+import calendarApi from "../../src/api/calendarApi";
+
+describe('Pruebas en el CalendarApi', () => {
+
+  test('Debe de tener la configuracion por defecto', () => {
+
+    expect( calendarApi.defaults.baseURL ).toBe( process.env.VITE_API_URL )
+
+  });
+
+  test('Debe de tener el x-token en el header de todas las peticiones', async() => {
+
+    const token = 'ABC-123-XYZ';
+    localStorage.setItem('token', token);
+    const res = await calendarApi.get('/auth');
+    
+    expect(res.config.headers['x-token']).toBe(token)
+
+  })
+
+})
